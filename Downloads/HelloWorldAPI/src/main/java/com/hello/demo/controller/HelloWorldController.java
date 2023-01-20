41ab84811af9454223228ab01e694953566e9def
Harrison Holsinger <harrisonholsinger@chenmed.com>
2023-01-20 11:51:34 -0500

    Error Controller

--- Downloads/HelloWorldAPI/src/main/java/com/hello/demo/controller/HelloWorldController.java
+++ Downloads/HelloWorldAPI/src/main/java/com/hello/demo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.hello.demo.controller;
 import com.hello.demo.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,9 @@ public class HelloWorldController {
     @Autowired
     private HelloWorldService service;
 
-    @GetMapping("/HelloWorld")
+    @PostMapping("/HelloWorld")
     String hello_world(){
         return service.Hello_World();
     }
 }
+
