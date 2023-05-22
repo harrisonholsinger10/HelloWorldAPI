5b5b397ff4bcccb5b86b22de017002acab69c42b
Harrison Holsinger <harrisonholsinger@gmail.com>
2023-05-22 14:26:18 -0400

    Delete Downloads/HelloWorldAPI directory

--- Downloads/HelloWorldAPI/src/main/java/com/hello/demo/controller/FallbackError.java
+++
@@ -1,5 +0,0 @@
-package com.hello.demo.controller;
-
-public interface FallbackError {
-    String getErrorPath();
-}
--- Downloads/HelloWorldAPI/src/main/java/com/hello/demo/repositories/HelloWorldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hello.demo.repositories;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloWorldRepository {
-}
